b55023d2a8f099ea9770277ea5e4a5e5dbc9132c
JESSE HUANG <jesshuang5733@gmail.com>
2023-10-07 15:14:17 -0400

    updated

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Hero from "./components/2_hero";
+
+function App() {
+  return (
+    <>
+      <Hero/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/2_hero/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <main>
+      <h2>Hey there! I'm</h2>
+      <h1>Jesse Huang</h1>
+    </main>
+  )
+}
+
+export default Hero
